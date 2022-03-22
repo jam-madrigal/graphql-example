21c3978d1210f4c4c0906e2eeaf528648b00b7a6
jam-madrigal <jam.madrigal@gmail.com>
2022-03-21 20:55:01 -0700

    Expand schema, update root

--- server.js
+++ server.js
@@ -3,16 +3,66 @@ const { buildSchema } = require('graphql');
 // An express middleware function that responds to graphql queries
 const { graphqlHTTP } = require('express-graphql');
 // Using the graphql function to build a schema for an ecommerce app. We define all the types of data we will have, and we always start with the query type, which every graphql service has. It defines the entry point of every query: it is the shape of the data that will be returned from our queries.
+// We can make types, which remind me of Classes, by doing what we did with Product and Review here. Adding ! after the type makes it a required field.
+// ID is a built in graphql type
 const schema = buildSchema(`
     type Query {
-        description: String
-        price: Float
+        products: [Product]
+    }
+
+    type Product {
+        id: ID!
+        description: String!,
+        price: Float!,
+        reviews: [Review]
+    }
+
+    type Review {
+        rating: Int!,
+        comment: String
+    }
+
+    type Order {
+        date: String!,
+        subtotal: Float!,
+        items: [OrderItem]
+    }
+
+    type OrderItem {
+        product: Product!,
+        quantity: Int!
     }
 `);
 // Static description and price values to use for now
 const root = {
-    description: 'Witch Hat',
-    price: 66.6
+    products: [
+        {
+            id: 'witchhat',
+            description: 'Witch Hat',
+            price: 13.37
+        },
+        {
+            id: 'twosidedukulele',
+            description: 'Two-sided Ukulele',
+            price: 52.44
+        }
+    ],
+    orders: [
+        {
+            date: '2021-11-27',
+            subtotal: 119.18,
+            items: [
+                {
+                    product: {
+                        id: 'twosidedukulele',
+                        description: 'Birthday Ukulele',
+                        price: 59.59
+                    },
+                    quantity: 2
+                }
+            ]
+        }
+    ]
 }
 
 const app = express();
