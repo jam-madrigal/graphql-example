b29d1c79c47df4d208777080becb3d7176a949db
jam-madrigal <jam.madrigal@gmail.com>
2022-03-23 23:43:26 -0700

    Simplify product by id query name

--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -7,7 +7,7 @@ module.exports = {
         },
         // The convention for using built in graphql arguments is to use _ if you are not using it in your callback, as shown here, we omit parent and put _ in its place
         // Get products by ID and by price range
-        productById: (_, args) => {
+        product: (_, args) => {
             return productsModel.getProductById(args.id);
         },
         productsByPrice: (_, args) => {
